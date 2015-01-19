387ecb2bdbc3537a8ac5e8c5b95818780210f232
Francesco Bigagnoli <francesco.bigagnoli@subito.it>
2015-01-19 11:37:50 +0100

    Created proj and added .gitignore file
    Signed-off-by: Francesco Bigagnoli <francesco.bigagnoli@subito.it>

---
+++ MarvelBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarvelBrowser
+//
+//  Created by francesco bigagnoli on 19/01/15.
+//  Copyright (c) 2015 francesco bigagnoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarvelBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarvelBrowser
+//
+//  Created by francesco bigagnoli on 19/01/15.
+//  Copyright (c) 2015 francesco bigagnoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
